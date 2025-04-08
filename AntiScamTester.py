8d0197a1ed95655216a1e4ce5afd88305288764f
HiHiYoyo606_PC <kyuchiang@gmail.com>
2025-04-09 00:08:50 +0800

    nothing

--- AntiScamTester.py
+++ AntiScamTester.py
@@ -217,13 +217,13 @@ def main():
 
     except Exception as e:
         st.session_state.last_message = ""
-        show_error(f"原因 Reason: {e}")
         information = [
             "如果下方警告訊息為\"Event loop is closed\", 只需再按一次按鈕即可。",
             "If the error message below is \"Event loop is closed\", just click the button again."
         ]
 
         st.toast("\n".join(information), icon="ℹ️")
+        show_error(f"原因 Reason: {e}")
 
 if __name__ == "__main__":
     main()
